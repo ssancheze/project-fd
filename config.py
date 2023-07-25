5c25109d9fbeb5916a51e082c5bf25f99ea2f8b1
thesersaer <sersaer@gmail.com>
2023-07-25 19:58:34 +0200

    Add map offline mode setting

--- config.py
+++ config.py
@@ -2,15 +2,21 @@
 manually, as any changes to the parameters' keys will render the code unusable, be sure to only modify the values,
 not the keys."""
 
-windowed_resolution = (1280, 720)
+# ____ WINDOW SETTINGS ____
 # Int tuple of format (width: int, height: int)
+win_windowed_resolution = (1280, 720)
 
-fullscreen = True
 # Whether the app starts in fullscreen mode or not
+win_fullscreen = True
 
-resizable = ""
 # String of value '' for none, 'h' for horizontal only, 'v' for vertical only or 'hv' / 'vh' for both.
 # WARNING: may produce funky results.
+win_resizable = ""
 
-borderless = False
 # Sets the window border
+win_borderless = False
+
+
+# ____ MAP SETTINGS ____
+# Sets whether the map is loaded from the Internet or the local database
+map_offline_mode = True
