7c0f58bf32c1f016ffbd12d752c737c092843fdf
thesersaer <sersaer@gmail.com>
2023-08-17 19:35:40 +0200

    Add DroneLab coordinates

--- definitions.py
+++ definitions.py
@@ -2,7 +2,10 @@ import os
 """
 
 """
-
+# DIRECTORIES
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 MAPS_DIR = os.path.join(ROOT_DIR, r'model\maps')
 FENCES_DIR = os.path.join(ROOT_DIR, r'fences')
+
+# VALUES
+EETAC_COORDINATES = 41.27641629296008, 1.9886751866535248
