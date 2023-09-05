0a27cd8463f8b1c6094541dd81646c7a6cb903e8
thesersaer <sersaer@gmail.com>
2023-09-05 12:23:00 +0200

    Add file

---
+++ controller/FrameControllers.py
@@ -0,0 +1,14 @@
+
+
+class WelcomeFrameController:
+    def __init__(self):
+        pass
+
+    def start_button_click(self):
+        pass
+
+    def settings_button_click(self):
+        pass
+
+    def credits_button_click(self):
+        pass
