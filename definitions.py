cc0e85739a6d15613dd593eeec250576d8ed2468
thesersaer <sersaer@gmail.com>
2023-09-05 12:23:39 +0200

    Add definitions and format

--- definitions.py
+++ definitions.py
@@ -2,10 +2,68 @@ import os
 """
 
 """
-# DIRECTORIES
+
+# ____DIRECTORIES
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 MAPS_DIR = os.path.join(ROOT_DIR, r'model\maps')
+
 FENCES_DIR = os.path.join(ROOT_DIR, r'fences')
 
-# VALUES
+ASSETS_DIR = os.path.join(ROOT_DIR, r'assets')
+
+
+# ____VALUES
 EETAC_COORDINATES = 41.27641629296008, 1.9886751866535248
+
+MAX_RACERS = 2
+
+EARTH_RADIUS_METERS = 6371E3
+
+CHECKPOINT_SEPARATION_UNITS = 5
+
+CHECKPOINT_SEPARATION_INTERVAL_METERS = 0.1
+
+TELEMETRY_RATE_HZ = 8
+
+LOOP_CEILING = 10000
+
+
+# ____AUTOPILOT
+SITL_PORT = 5763
+
+SITL_ADDRESS = 'localhost'
+
+SITL_PROTOCOL = 'tcp'
+
+MQTT_INTERNAL_PORT = 1884
+
+MQTT_EXTERNAL_PORT = 8000
+
+RPI_PORT_ADDRESS = '/dev/ttyS0'
+
+TELEMETRY_BAUD = 57600
+
+DEFAULT_BAUD = 115200
+
+# ____TK
+POLYGON_IDLE_KWARGS = {
+    'fill_color': None,
+    'outline_color': '#F7B801',
+    'border_width': 2,
+
+}
+
+POLYGON_INCLUSION_KWARGS = {
+    'fill_color': '#4A5859',
+    'outline_color': '#F4D6CC',
+    'border_width': 2,
+
+}
+
+POLYGON_EXCLUSION_KWARGS = {
+    'fill_color': '#C83E4D',
+    'outline_color': '#DD1C1A',
+    'border_width': 2,
+
+}
